836d0a46fe245ac60f3f47eb1fb9001a7b98816a
saimadhukarsomu@gmail.com <saimadhukarsomu@gmail.com>
2025-05-05 01:50:16 -0500

    Rect corner Radius

--- Sources/SpaceFoundation/Extensions/ViewExtensions.swift
+++ Sources/SpaceFoundation/Extensions/ViewExtensions.swift
@@ -54,6 +54,10 @@ extension View {
                 .presentationDragIndicator(.visible)
         }
     }
+    
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
 }
 
 private struct _CustomForeground<Content: View, Overlay: View>: View {
@@ -67,5 +71,23 @@ private struct _CustomForeground<Content: View, Overlay: View>: View {
     
     var body: some View {
         content.overlay(overlay).mask(content)
+    }
+}
+
+struct RoundedCorner: Shape {
+    let radius: CGFloat
+    let corners: UIRectCorner
+
+    init(radius: CGFloat = .infinity, corners: UIRectCorner = .allCorners) {
+        self.radius = radius
+        self.corners = corners
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: corners,
+                                cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
     }
 }
