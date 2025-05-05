7ed0469cc7653ff6c690da875b3fbfb0bd101e99
saimadhukarsomu@gmail.com <saimadhukarsomu@gmail.com>
2025-05-05 15:15:03 -0500

    TabView changes

--- Sources/SpaceFoundation/Components/TabBar/SpaceTabBar.swift
+++ Sources/SpaceFoundation/Components/TabBar/SpaceTabBar.swift
@@ -8,17 +8,27 @@
 import Foundation
 import SwiftUI
 
-public enum SpaceTabItem: String, CaseIterable, Identifiable, CustomStringConvertible, Equatable {
-    case home = "house.fill"
-    case saved = "heart.fill"
-    case profile = "person.fill"
+public enum SpaceTabItem: String, RawRepresentable, CaseIterable, Identifiable, CustomStringConvertible, Equatable {
+    case home = "Home"
+    case explore = "Explore"
+    case favourite = "Favourite"
+    case profile = "Profile"
     
     public var id: String {
         return self.rawValue
     }
     
     public var description: String {
-        return self.rawValue
+        switch self {
+        case .home:
+            return "house.fill"
+        case .explore:
+            return "sparkle.magnifyingglass"
+        case .favourite:
+            return "heart.fill"
+        case .profile:
+            return "person.fill"
+        }
     }
     
     public static func == (lhs: SpaceTabItem, rhs: SpaceTabItem) -> Bool {
@@ -26,40 +36,40 @@ public enum SpaceTabItem: String, CaseIterable, Identifiable, CustomStringConver
     }
 }
 
-public struct SpaceTabBar<CustomTab: Identifiable & CustomStringConvertible & Equatable>: View {
+public struct SpaceTabBar<CustomTab: Identifiable & RawRepresentable & CustomStringConvertible & Equatable>: View {
     @Binding var selectedTab: CustomTab
     @State var tabValues: [CustomTab]
     @State var animated: Bool = false
+    @State var height: CGFloat
     
-    public init(reqTabs: [CustomTab], selectedTab: Binding<CustomTab>) {
+    public init(reqTabs: [CustomTab], selectedTab: Binding<CustomTab>, height: CGFloat = 44) {
         self.tabValues = reqTabs
         _selectedTab = selectedTab
+        self.height = height
     }
     
     public var body: some View {
-        HStack {
+        HStack(spacing: 16) {
             ForEach(tabValues, id: \.id) { tab in
                 Spacer()
                 Button(action: {
                     selectedTab = tab
-                    withAnimation(.spring(duration: 1)) {
+                    withAnimation(.easeInOut(duration: 1.0)) {
                         animated.toggle()
                     }
                 }) {
-                    VStack {
-                        Image(systemName: tab.description)
-                            .font(.system(size: 24))
-                            .scaleEffect(animated && selectedTab == tab ? 0.90 : 1)
-                            .shadow(radius: animated && selectedTab == tab ? 4 : 0)
-                            .foregroundColor(selectedTab == tab ? SpaceColors.yellow90 : SpaceColors.white)
-                            .padding(8)
-                            .clipShape(Circle())
-                    }
+                    Image(systemName: tab.description)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 18, height: 18)
+                        .scaleEffect(animated && selectedTab == tab ? 1.05 : 1)
+                        .shadow(radius: animated && selectedTab == tab ? 4 : 0)
+                        .foregroundColor(selectedTab == tab ? SpaceColors.yellow75 : SpaceColors.white)
                 }
                 Spacer()
             }
         }
-        .padding(.vertical, 8)
+        .frame(height: height)
         .background(Color.primary)
         .cornerRadius(32)
         .padding(.horizontal, 24)
